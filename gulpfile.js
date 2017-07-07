1c04c4856a18041effc1b240743e98a9caa6c919
mohammed <mohammed.echariq@gmail.com>
2017-07-07 20:36:52 +0100

    concat script files at single file

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,22 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util')
-    coffee = require('gulp-coffee');
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat');
 
+var coffeeResources = ['components/coffee/tagline.coffee'];
+var scriptResources = [
+    'components/scripts/pixgrid.js',
+    'components/scripts/rclick.js',
+    'components/scripts/tagline.js',
+    'components/scripts/template.js'];
 gulp.task('coffee', function () {
-    gulp.src('components/coffee/tagline.coffee')
+    gulp.src(coffeeResources)
         .pipe(coffee({bare: true})
             .on('error', gutil.log))
         .pipe(gulp.dest('components/scripts'))
+});
+gulp.task('js', function () {
+    gulp.src(scriptResources)
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('builds/development/js'))
 });
