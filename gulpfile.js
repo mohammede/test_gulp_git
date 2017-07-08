40f23be7d3e572ccb03a27e56888455c1b47f307
mohammed <mohammed.echariq@gmail.com>
2017-07-08 18:33:57 +0100

    add libraries jquery and mustache with Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    browserify = require('gulp-browserify');
 
 var coffeeResources = ['components/coffee/tagline.coffee'];
 var scriptResources = [
@@ -18,5 +19,6 @@ gulp.task('coffee', function () {
 gulp.task('js', function () {
     gulp.src(scriptResources)
         .pipe(concat('script.js'))
+        .pipe(browserify())
         .pipe(gulp.dest('builds/development/js'))
 });
